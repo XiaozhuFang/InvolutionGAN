eeb5c0b07fabf7ce7f1d81ab8d72319ba80d37c0
XiaozhuFang <fangxiaozhu2014@gmail.com>
2021-05-02 15:35:50 +0800

    Initialization

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="involution",
+    version="0.1",
+    url="https://github.com/ChristophReich1996/Involution",
+    license="MIT License",
+    author="Christoph Reich",
+    author_email="ChristophReich@gmx.net",
+    description="PyTorch 2d Involution",
+    packages=["involution",],
+    install_requires=["torch>=1.7.0"],
+)
